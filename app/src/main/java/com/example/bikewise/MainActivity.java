d7f33b6e3b2e8280278d41d06bee3e4c5a64ef1d
Xilth <el.jason156@gmail.com>
2021-01-11 20:25:10 +0700

    Revert "[UPDATE] Fitur Login - Prototype"
    This reverts commit 6cd847ee88694678ceaaaba69f1c4650c1ff6994.

--- app/src/main/java/com/example/bikewise/MainActivity.java
+++ app/src/main/java/com/example/bikewise/MainActivity.java
@@ -2,24 +2,14 @@ package com.example.bikewise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     RelativeLayout signUpExitSection, rellay1;
-
-    EditText email, pass;
-    Button login;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -39,24 +29,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signUpExitSection = (RelativeLayout) findViewById(R.id.signUpExitSection);
 
         handler.postDelayed(runnable, 2000);
-
-        this.email = (EditText) findViewById(R.id.et_email);
-        this.pass = (EditText) findViewById(R.id.et_password);
-        this.login = (Button) findViewById(R.id.btn_login);
-
-        this.login.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        String toastMsg = "";
-        if(this.email.getText().length() > 0 && this.pass.getText().length() > 0){
-            //Login Fitur sementara. Nantinya bisa menggunakan presenter untuk menyimpan email dan password sesuai index bila matching
-            toastMsg = this.email.getText().toString() + " has Logged In Successfully";
-            Toast.makeText(this, toastMsg, Toast.LENGTH_SHORT).show();
-        } else {
-            toastMsg = "Email or Passowrd is Incorrect!";
-            Toast.makeText(this, toastMsg, Toast.LENGTH_SHORT).show();
-        }
     }
 }
